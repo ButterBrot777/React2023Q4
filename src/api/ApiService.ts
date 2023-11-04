db5057ac0f4eb69ad8a8de0c96891ee57fdbbf23
Karenda <korendos@gmail.com>
2023-11-04 23:27:25 +0300

    feat: add select items per page

--- src/api/ApiService.ts
+++ src/api/ApiService.ts
@@ -1,26 +1,10 @@
-import axios, { AxiosError } from "axios";
-const BASE_URL = "https://dummyjson.com/products/";
+import axios from "axios";
+const BASE_URL = "https://dummyjson.com/products";
 
 export class ApiService {
-  public static async getItems(limit = 10, query = "") {
-    try {
-      const response = await axios.get(
-        `${BASE_URL}?limit=${limit}&search?q=${query}`
-      );
-      console.log("response: ", response);
-
-      return response.data.products;
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        const axiosError = error as AxiosError;
-        console.error("API request failed:", axiosError.message);
-
-        if (axiosError.response?.status === 404) {
-          throw new Error("Bad request. Please check URL params.");
-        }
-      }
-
-      throw error;
-    }
+  public static async getItems(limit = "10", query: string, skip = "") {
+    const url = `${BASE_URL}/search?q=${query}&limit=${limit}&skip=${skip}`;
+    console.log("url: ", url);
+    return axios.get(url);
   }
 }
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { ApiService } from "../api/ApiService.ts";
 
 interface Product {
@@ -16,61 +16,81 @@ interface Product {
 }
 
 export const SearchPage = () => {
+  useEffect(() => {
+    const savedSearchTerm = localStorage.getItem("searchTerm");
+    if (savedSearchTerm) setSearchTerm(savedSearchTerm);
+  }, []);
+
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState<Product[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<Error | null>(null);
-  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [itemsPerPage, setItemsPerPage] = useState("10");
+  const ref = useRef<HTMLInputElement | null>(null);
 
   const loadSearchResults = useCallback(() => {
     setLoading(true);
     ApiService.getItems(itemsPerPage, searchTerm)
       .then((data) => {
         console.log("data: ", data);
-        setSearchResults(data);
+        setSearchResults(data?.data.products);
+        setError(null);
       })
       .catch((error) => setError(error))
-      .finally(() => setLoading(false));
-  }, [searchTerm]);
+      .finally(() => {
+        console.log("term: ", searchTerm);
+        setLoading(false);
+      });
+  }, [itemsPerPage, searchTerm]);
 
   useEffect(() => {
-    const savedSearchTerm = localStorage.getItem("searchTerm");
-    if (savedSearchTerm) {
-      setSearchTerm(savedSearchTerm);
-      loadSearchResults();
-    } else {
-      loadSearchResults();
-    }
-  }, [setSearchTerm]);
-
-  const handleSearchInputChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setSearchTerm(event.target.value);
-  };
+    loadSearchResults();
+  }, [loadSearchResults]);
 
   const handleEnter = (event: React.KeyboardEvent) => {
     if (event.key === "Enter") handleSearch();
   };
 
   const handleSearch = () => {
-    const trimmedSearchTerm = searchTerm.trim();
-    localStorage.setItem("searchTerm", trimmedSearchTerm);
-    loadSearchResults();
+    const currentRef = ref.current?.value;
+    if (currentRef) {
+      const value = currentRef.trim();
+      setSearchTerm(value);
+      localStorage.setItem("searchTerm", value);
+      loadSearchResults();
+    } else {
+      localStorage.setItem("searchTerm", "");
+      console.log("empty");
+      setSearchTerm("");
+      loadSearchResults();
+    }
   };
 
   const throwTestError = () => {
-    loadSearchResults("asdf");
+    throw new Error("Test error buuuaaaa!!!");
+    setError(new Error("Test error buuuaaaa!!!"));
+  };
+
+  const handlePrev = () => {
+    console.log("prev: ", itemsPerPage);
+  };
+  const handleNext = () => {
+    console.log("next: ", itemsPerPage);
+  };
+  const handleItemsCountChange = async (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const value = event.target.value;
+    setItemsPerPage(value);
   };
 
   return (
     <div>
       <div style={{ marginBottom: "16px" }}>
         <input
-          type="text"
-          value={searchTerm}
-          onChange={handleSearchInputChange}
+          defaultValue={localStorage.getItem("searchTerm") ?? ""}
           onKeyDown={handleEnter}
+          ref={ref}
         />
         <button onClick={handleSearch} disabled={loading}>
           Search
@@ -90,6 +110,22 @@ export const SearchPage = () => {
           ))}
         </ul>
       )}
+      <div className="flex">
+        <button type="button" onClick={handlePrev}>
+          prev
+        </button>
+        <select
+          className="flex"
+          value={itemsPerPage}
+          onChange={handleItemsCountChange}
+        >
+          <option value={"5"}>5</option>
+          <option value={"10"}>10</option>
+        </select>
+        <button type="button" onClick={handleNext}>
+          next
+        </button>
+      </div>
     </div>
   );
 };
