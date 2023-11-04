138c7d424467f8466d98f330fc05288a3fdd7f42
Karenda <korendos@gmail.com>
2023-11-05 01:50:38 +0300

    feat: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import { SearchPage } from "./pages/SearchPage.tsx";
 import ErrorBoundary from "./components/ErrorBoundary.tsx";
+import React from "react";
 
 export const App = () => {
   return (
     <>
+        <React.StrictMode>
       <ErrorBoundary>
         <SearchPage />
       </ErrorBoundary>
+
+        </React.StrictMode>
     </>
   );
 };
